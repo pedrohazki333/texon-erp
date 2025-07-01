0b036e525b3fdf7a0b791b69dc2dd405064338bb
Pedro <pedrohazki.emp@gmail.com>
2025-07-01 09:27:58 -0300

    Add dashboard controller, view, and tests

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('layout');
-});
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::resource('customers', CustomerController::class);
 Route::resource('products', ProductController::class);
